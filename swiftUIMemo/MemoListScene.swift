5a4a69a67a0a5daf5dc4b25eb7649c608ec82687
Taeangel <wngks2050@gmail.com>
2022-06-29 07:54:54 +0900

    feat: MemoListScene에서 EnvironmentObject 등록

--- swiftUIMemo/MemoListScene.swift
+++ swiftUIMemo/MemoListScene.swift
@@ -4,17 +4,25 @@
 //
 //  Created by song on 2022/06/29.
 //
-    
+
 import SwiftUI
 
 struct MemoListScene: View {
-    var body: some View {
-      Text("핼로월드!!")
+  @EnvironmentObject var store: MemoStore // 하나의 데이터를 여러군데서 공유함
+  
+  var body: some View {
+    NavigationView {
+      List(store.list) { memo in
+        Text(memo.content)
+      }
+      .navigationTitle("내 메모")
     }
+  }
 }
-   
+
 struct MemoListScene_Previews: PreviewProvider {
-    static var previews: some View {
-      MemoListScene()
-    }
+  static var previews: some View {
+    MemoListScene()
+      .environmentObject(MemoStore())
+  }
 }
